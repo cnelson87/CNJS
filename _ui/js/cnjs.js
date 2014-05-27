8f118f6d795beebe12112bd36d74cc0c4d2d5e1e
Chris Nelson <cnelson87@gmail.com>
2014-05-27 16:29:43 -0700

    add pseudo ajax content

--- _ui/js/cnjs.js
+++ _ui/js/cnjs.js
@@ -5,11 +5,8 @@
 var CNJS = CNJS || {};
 CNJS.Config = {
 	siteRoot: 'http://localhost:8888/cnjs/',
-	isIE:  !!(navigator.userAgent.indexOf('MSIE') >= 0),
-	isIE7: !!(navigator.userAgent.indexOf('MSIE 7') >= 0),
-	isIE8: !!(navigator.userAgent.indexOf('MSIE 8') >= 0),
-	isIE9: !!(navigator.userAgent.indexOf('MSIE 9') >= 0),
-	isIDevice: (navigator.platform.indexOf('iPhone')>=0 || navigator.platform.indexOf('iPad')>=0) ? true : false,
+	isIE9: (navigator.userAgent.indexOf('MSIE 9') >= 0) ? true : false,
+	isIDevice: (navigator.platform.indexOf('iPhone') >= 0 || navigator.platform.indexOf('iPad') >= 0) ? true : false,
 	isIPhone: (navigator.userAgent.match(/iPhone/i) !== null) ? true : false,
 	isIPad: (navigator.userAgent.match(/iPad/i) !== null) ? true : false,
 	defaultAjaxErrorMessage: '<div class="errormessage"><p>Sorry. Ajax request failed.</p></div>'
